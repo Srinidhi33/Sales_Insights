609744b7db184c9315457db0ef53500d3917a2c2
SRINIDHI <srinidhiin33@gmail.com>
2024-07-15 01:51:37 +0530

    requirements.txt

--- data_exctractor.py
+++ data_exctractor.py
@@ -1,8 +1,13 @@
 import mysql.connector
 import pandas as pd
-mydb = mysql.connector.connect(user='root', password='srinidhi', host='localhost', port=3306, database='sales')
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM markets")
-myresult = mycursor.fetchall()
-markets_data = pd.DataFrame(myresult)
-print(markets_data.shape)
+
+
+tables=['customers','date','markets','products','transactions']
+
+cnx = mysql.connector.connect(user='root', password='srinidhi', host='localhost', port=3306, database='sales')
+
+
+for table_name in tables:
+    query=f"Select * from {table_name}"
+    df=pd.read_sql_query(query,cnx)
+    df.to_csv(f"datasets/{table_name}.csv")
