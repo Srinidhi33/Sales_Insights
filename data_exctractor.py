e5a57b6a26372ecb36d3104e5305ba7b1cc2d125
SRINIDHI <srinidhiin33@gmail.com>
2024-07-15 01:17:53 +0530

    Initial Commit

---
+++ data_exctractor.py
@@ -0,0 +1,8 @@
+import mysql.connector
+import pandas as pd
+mydb = mysql.connector.connect(user='root', password='srinidhi', host='localhost', port=3306, database='sales')
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM markets")
+myresult = mycursor.fetchall()
+markets_data = pd.DataFrame(myresult)
+print(markets_data.shape)
